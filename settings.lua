e1c39c1e8d7b3d356f8777ab7004674712d3ce03
Talandar <talandar66@protonmail.com>
2026-01-21 22:23:54 +0100

    cargo crates spoilables support

--- settings.lua
+++ settings.lua
@@ -15,10 +15,24 @@ data:extend({
 	},
 	{
 		type = "bool-setting",
-		name = "cargo-crates-can-use-regular-assembling-machines",
+		name = "cargo-crates-pack-spoilables",
 		setting_type = "startup",
 		default_value = true,
 		order = "cargo-crates-c",
+	},
+	{
+		type = "bool-setting",
+		name = "cargo-crates-pack-capsules",
+		setting_type = "startup",
+		default_value = true,
+		order = "cargo-crates-d",
+	},
+	{
+		type = "bool-setting",
+		name = "cargo-crates-can-use-regular-assembling-machines",
+		setting_type = "startup",
+		default_value = true,
+		order = "cargo-crates-e",
 		hidden = true,
 	},
 })
